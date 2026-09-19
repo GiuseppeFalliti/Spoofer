db372b76c208630b69be7f922626e11afa1292d7
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2026-09-19 10:46:04 +0200

    Keep system information proxy WDK compatible

--- driver/driver.h
+++ driver/driver.h
@@ -59,7 +59,7 @@ DRIVER_UNLOAD HwidUnload;
 
 NTSTATUS
 Hooked_NtQuerySystemInformation(
-    _In_ SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    _In_ ULONG SystemInformationClass,
     _Out_writes_bytes_opt_(SystemInformationLength) PVOID SystemInformation,
     _In_ ULONG SystemInformationLength,
     _Out_opt_ PULONG ReturnLength
